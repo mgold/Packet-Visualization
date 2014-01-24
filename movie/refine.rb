be88f50a2f6bd323b816b71636b02a71d6c85321
Max Goldstein <maxgoldstein1@gmail.com>
2014-01-23 20:02:04 -0500

    Kind of works

--- movie/refine.rb
+++ movie/refine.rb
@@ -12,10 +12,10 @@ dat_file = "movieData.csv"
 ips = File.new(ip_file).to_a.map{|line| line.chomp!}
 
 file = File.open(dat_file, mode="w")
-file.puts "ts,loc_id,ip_for,dir,proto"
+file.puts "ts,loc_id,ip_for,dir,proto,len"
 
-CSV.foreach(sel_file) do |ts,ip_loc,ip_for,dir,proto|
-    ip_for_key = (IPAddr.new(ip_for).to_i && 0xFFFF0000) >> 16
+CSV.foreach(sel_file) do |ts,ip_loc,ip_for,dir,proto,len|
+    ip_for_key = (IPAddr.new(ip_for).to_i & 0xFFFF0000) >> 16
     if proto.start_with?("HTTP") then proto = "HTTP" end
-    file.puts [ts,ips.index(ip_loc),ip_for_key,dir[0],proto].join(",")
+    file.puts [ts,ips.index(ip_loc),ip_for_key,dir[0],proto,len].join(",")
 end
